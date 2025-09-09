3d5445012e73865c2c4129927725dff3cc925670
folcksage <anton.gulev@gmail.com>
2025-09-10 00:17:38 +0300

    bdsm

---
+++ Lab2/Lab2.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+class Integer
+{
+public:
+	Integer(int number)
+	{
+		value = number;
+	}
+	Integer& add(const Integer& obj)
+	{
+		value += obj.value;
+		return *this;
+	}
+	Integer& subtract(const Integer& obj)
+	{
+		value -= obj.value;
+		return *this;
+	}
+	Integer& multiply(const Integer& obj)
+	{
+		value *= obj.value;
+		return *this;
+	}
+	void print() const
+	{
+		std::cout << "Value: " << value << std::endl;
+	}
+private:
+	int value;
+};
+void main() {
+	Integer num{ 10 };
+	num.add(Integer{ 30 }).subtract(Integer{ 15 }).multiply(Integer{ 2 });
+	num.print(); // Value: 50
+}
